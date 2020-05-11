817c5195477af8fab06fd39bbd0bf971f910e6c7
Anton Shelenkov <anshelen@yandex.ru>
2020-05-11 16:08:47 +0300

    feat: Add logging

--- src/main/java/dev/shelenkov/microservicesgateway/web/EndpointController.java
+++ src/main/java/dev/shelenkov/microservicesgateway/web/EndpointController.java
@@ -2,12 +2,14 @@ package dev.shelenkov.microservicesgateway.web;
 
 import dev.shelenkov.microservicesgateway.adapter.BackendAdapter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class EndpointController {
 
     private final BackendAdapter backendAdapter;
@@ -20,7 +22,9 @@ public class EndpointController {
 
     @GetMapping("/")
     public String getRequestsCount() {
-        return String.format("Number of requests %s (gateway %d, secret %s)",
+        String result = String.format("Number of requests %s (gateway %d, secret %s)",
             backendAdapter.getRequests(), instanceId, secret);
+        log.info("Handling count request. Result {}", result);
+        return result;
     }
 }
